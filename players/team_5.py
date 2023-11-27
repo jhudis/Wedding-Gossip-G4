19d1b3268032e6a0f050b015ff58de4cb16139c1
Anubhav Jangra <anubhav0603@gmail.com>
2023-11-26 23:18:46 -0500

    update MARL implementation (still broken)

--- players/team_5.py
+++ players/team_5.py
@@ -2,7 +2,7 @@ import random
 import functools
 import numpy as np
 
-import gymnasium as gym
+import gymnasium
 from gymnasium.spaces import Discrete
 
 from pettingzoo import AECEnv
@@ -35,7 +35,7 @@ class Player():
         # a hash mapping player_ids to where they are situated on the board
         # each player_id is mapped to (table_num, seat_num)
         # initialized to (None, None)
-        self.player_seat_map = {p_id: (None, None) for i in range(90)}
+        self.player_seat_map = {p_id: (None, None) for p_id in range(90)}
 
         # action_to_num - to convert action into numerical value for model training purposes
         # can change the values to larger difference if this affects the model training
@@ -136,25 +136,27 @@ def reward_func(_states, _action, player_info):
 
 
 # dicts to convert actions into human readable informaiton
+DONT_KNOW = 5
 POSSIBLE_ACTIONS = {1: 'speak-left',
                     2: 'speak-right',
                     3: 'listen-left',
                     4: 'listen-right',
-                    5: 'move-1',
-                    6: 'move-2',
-                    7: 'move-3',
-                    8: 'move-4',
-                    9: 'move-5',
-                    10: 'move-6',
-                    11: 'move-7',
-                    12: 'move-8',
-                    13: 'move-9',
-                    14: 'move-10'}
+                    5: "don't know",
+                    6: 'move-1',
+                    7: 'move-2',
+                    8: 'move-3',
+                    9: 'move-4',
+                    10: 'move-5',
+                    11: 'move-6',
+                    12: 'move-7',
+                    13: 'move-8',
+                    14: 'move-9',
+                    15: 'move-10'}
 
-POSSIBLE_OBSERVATIONS = {1: 'speak left',
-                    2: 'speak right',
-                    3: 'listen left',
-                    4: 'listen right',
+POSSIBLE_OBSERVATIONS = {1: 'speak-left',
+                    2: 'speak-right',
+                    3: 'listen-left',
+                    4: 'listen-right',
                     5: "don't know"}
 
 class game_env(AECEnv):
@@ -167,7 +169,7 @@ class game_env(AECEnv):
 
     metadata = {"render_modes": ["human"], "name": "wedding_gossip_v1"}
 
-    def __init__(self, render_mode=None):
+    def __init__(self, render_mode=None, termination_limit=100):
         """
         The init method takes in environment arguments and
          should define the following attributes:
@@ -199,6 +201,9 @@ class game_env(AECEnv):
         }
 
         self.render_mode = render_mode
+
+        # termination after this number of steps
+        self.termination_limit = termination_limit
 
     # Observation space should be defined here.
     # lru_cache allows observation and action spaces to be memoized, reducing clock cycles required to get each agent's space.
@@ -270,14 +275,36 @@ class game_env(AECEnv):
         self.terminations = {agent: False for agent in self.agents}
         self.truncations = {agent: False for agent in self.agents}
         self.infos = {agent: {} for agent in self.agents}
-        self.state = {agent: NONE for agent in self.agents}
-        self.observations = {agent: NONE for agent in self.agents}
+        self.state = {agent: DONT_KNOW for agent in self.agents}
+        self.observations = {agent: DONT_KNOW for agent in self.agents}
         self.num_moves = 0
+
+        # each tuple stores the following for seat 'seat_num' at table 'table_num'
+        # 0 index - last observed action done by the player at seat_id (initialized to None) 
+        #           can be 'speak-left', 'speak-right' 'listen-left', 'listen-'right' or 'don't know'
+        #           check out POSSIBLE_OBSERVATIONS for exact mapping
+        # 2 index - player_id of last observed player at the seat (initialized to None)
+        # 3 index - time_stamp of last observation (initialized to -1)
+        self._wedding_state = {seat_id: (DONT_KNOW, None, -1) for seat_id in range(100)}
+
+        # a hash mapping player_ids to where they are situated on the board
+        # each player_id is mapped to a number from 0-99. 
+        # table_num = value % 10 (0-9)
+        # seat_num = value / 10 (0-9)
+        # initialized to None
+        self.player_seat_map = {p_id: None for p_id in range(90)}
+
         """
         Our agent_selector utility allows easy cyclic stepping through the agents list.
         """
         self._agent_selector = agent_selector(self.agents)
         self.agent_selection = self._agent_selector.next()
+
+    def obtain_observations(self, _action, _player):
+        """
+            Use the class variables to obtain the observations given an action taken by the _player (i.e. agent).
+        """
+        pass
 
     def step(self, action):
         """
@@ -289,6 +316,7 @@ class game_env(AECEnv):
         - truncations
         - infos
         - agent_selection (to the next agent)
+        - state
         And any internal state used by observe() or render()
         """
         if (
@@ -307,6 +335,34 @@ class game_env(AECEnv):
         # (because it was returned by last()), so the _cumulative_rewards for this
         # agent should start again at 0
         self._cumulative_rewards[agent] = 0
+
+        # stores action of current agent
+        self.state[self.agent_selection] = action
+
+        # collect reward if it is the last agent to act
+        if self._agent_selector.is_last():
+            # rewards for all agents are placed in the .rewards dictionary
+            self.rewards = reward_func(self.player_seat_map, self._wedding_state)
+
+            self.num_moves += 1
+            # The truncations dictionary must be updated for all players.
+            self.truncations = {
+                agent: self.num_moves >= self.termination_limit for agent in self.agents
+            }
+
+            # observe the current state
+            for i in self.agents:
+                self.observations[i] = self.state[
+                    self.agents[1 - self.agent_name_mapping[i]]
+                ]
+        else:
+            # necessary so that observe() returns a reasonable observation at all times.
+            self.state[self.agents[1 - self.agent_name_mapping[agent]]] = NONE
+            # no rewards are allocated until both players give an action
+            self._clear_rewards()
+
+        if self.render_mode == "human":
+            self.render()
 
 
 # POSSIBLE_ACTIONS = {('speak', 'left'): 1,
